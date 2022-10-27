aeb1a59d708fa111d4b2169d21bdf0e0b1640b20
JustOptimize <mirko303.staff@gmail.com>
2022-10-27 17:20:40 +0200

    Added old lock icon, modified settings  :100:

--- ShowHiddenChannels.plugin.js
+++ ShowHiddenChannels.plugin.js
@@ -1,7 +1,7 @@
 /**
  * @name ShowHiddenChannels
  * @displayName Show Hidden Channels (SHC)
- * @version 0.0.5
+ * @version 0.0.6
  * @author JustOptimize (Oggetto)
  * @authorId 347419615007080453
  * @source https://github.com/JustOptimize/return-ShowHiddenChannels
@@ -17,12 +17,18 @@ module.exports = (() => {
         name: "JustOptimize (Oggetto)",
       }],
       description: "A plugin which displays all hidden Channels, which can't be accessed due to Role Restrictions, this won't allow you to read them (impossible).",
-      version: "0.0.5",
+      version: "0.0.6",
       github: "https://github.com/JustOptimize/return-ShowHiddenChannels",
       github_raw: "https://raw.githubusercontent.com/JustOptimize/return-ShowHiddenChannels/main/ShowHiddenChannels.plugin.js"
     },
 
     changelog: [
+      {
+        title: "v0.0.6",
+        items: [
+          "Added back the old lock icon and modified settings to be more user friendly.",
+        ]
+      },
       {
         title: "v0.0.5",
         items: [
@@ -52,6 +58,13 @@ module.exports = (() => {
         value: false
       },
       {
+        type: "switch",
+        id: "useIconsV2",
+        name: "Use V2 Icons",
+        note: "Use the old ShowHiddenChannels icons. (Not customizable and bad performance while scrolling)",
+        value: true
+      },
+      {
         type: "textbox",
         id: "emoji",
         name: "Locked Channel Emoji/Text",
@@ -63,6 +76,13 @@ module.exports = (() => {
         id: "OnRight",
         name: "Emoji/Text on the right",
         note: "This setting changes the position of the lock icon to the left or right side of the channel name.",
+        value: false
+      },
+      {
+        type: "switch",
+        id: "MarkUnread",
+        name: "Stop marking as read hidden channels",
+        note: "This setting stops the plugin from marking hidden channels as read.",
         value: false
       },
       {
@@ -137,12 +157,8 @@ module.exports = (() => {
       PluginUpdater,
       Logger,
       Modals,
-      // Utilities,
       DiscordModules: {
         MessageActions,
-        // React,
-        // Tooltip,
-        // Clickable
       }
     } = Library;
 
@@ -151,33 +167,16 @@ module.exports = (() => {
     const DiscordConstants = WebpackModules.getModule((m) => m?.Plq?.ADMINISTRATOR == 8n);
     const ChannelPermissionStore = WebpackModules.getByProps("getChannelPermissions");
     const UnreadStore = WebpackModules.getByProps("isForumPostUnread");
-    // const ChannelItem = WebpackModules.getByString("canHaveDot", "unreadRelevant", "UNREAD_HIGHLIGHT")
-    // const ChannelClasses = WebpackModules.getByProps("wrapper", "mainContent");
-    // const { iconItem, iconBase, actionIcon } = WebpackModules.getByProps("iconItem"); //iconItem-1EjiK0 iconBase-2G48Fc actionIcon-2sw4Sl
+    const ChannelItem = WebpackModules.getByString("canHaveDot", "unreadRelevant", "UNREAD_HIGHLIGHT")
+    const SomeStore = WebpackModules.getByProps("hasUnread");
 
-    // const registry = WebpackModules.getModules((m) => typeof m === "function" && m.toString().indexOf('"currentColor"') !== -1);
-    //TODO: Icon not working
-    // const Icon = (props) => {
-    //   // const mdl = registry.find((m) => m.displayName === props.name);
-    //   const Icon = "link?"; //! link
-    //   const newProps = global._.cloneDeep(props);
-    //   delete newProps.name;
-      
-    //   return React.createElement(Icon, newProps);
-    // };
-
-    // console.log(registry);
-    // Icon.Names = registry.map((m) => m.name); //! displayName got removed so need to find unique identifier for the icon
-    // console.log(Icon.Names)
-    
-    
     // //TODO, Coming soon
     // const Route = WebpackModules.getModule((m) => m?.default?.toString().includes("impression"));
     // const ChannelUtil = WebpackModules.getByProps("selectChannel", "selectPrivateChannel");
     // const VoiceUser = WebpackModules.getByPrototypes("renderPrioritySpeaker", "renderIcons", "renderAvatar")
     // const VoiceUsers = WebpackModules.getByString("hidePreview", "previewIsOpen", "previewUserIdAfterDelay");
     // const ChannelContextMenu = WebpackModules.getByProps("openContextMenu");
-    
+
     return class ShowHiddenChannels extends Plugin {
       constructor() {
         super();
@@ -209,7 +208,14 @@ module.exports = (() => {
       Patch() {
         if(this.settings.debugMode) {
           console.log("UnreadStore", UnreadStore);
-          console.log("ChannelStore", ChannelStore);
+          // console.log("ChannelStore", ChannelStore);
+          console.log("SomeStore", SomeStore);
+          // console.log("Channel", Channel);
+          // console.log("WebpackModules", WebpackModules.getAllModules());
+          // console.log(mod);
+          console.log("ChannelItem", ChannelItem);
+          // const mod = WebpackModules.
+          // console.log("thing", mod);
         }
 
         //* List of UnreadStore functions:
@@ -219,32 +225,43 @@ module.exports = (() => {
         // - hasRelevantUnread
         // - hasUnread
 
-        Patcher.after(UnreadStore, "getMentionCount", (_, args, res) => {
-          return ChannelStore.getChannel(args[0])?.isHidden() ? 0 : res;
-        });
+        if(!this.settings.MarkUnread) {
+          Patcher.after(UnreadStore, "getMentionCount", (_, args, res) => {
+            return ChannelStore.getChannel(args[0])?.isHidden() ? 0 : res;
+          });
 
-        Patcher.after(UnreadStore, "getUnreadCount", (_, args, res) => {
-          return ChannelStore.getChannel(args[0])?.isHidden() ? 0 : res;
-        });
+          Patcher.after(UnreadStore, "getUnreadCount", (_, args, res) => {
+            // console.log("getUnreadCount", args, res);
+            return ChannelStore.getChannel(args[0])?.isHidden() ? 0 : res;
+          });
 
-        //! Seems like this is not working anymore
-        // Patcher.after(UnreadStore, "hasAnyUnread", (_, args, res) => {
-        //   console.log("hasAnyUnread", args, res);
-        //   return res && !ChannelStore.getChannel(args[0])?.isHidden();
-        // });
+          //! Seems like this is not working anymore
+          Patcher.after(UnreadStore, "hasAnyUnread", (_, args, res) => {
+            console.log("hasAnyUnread", args, res);
+            return res && !ChannelStore.getChannel(args[0])?.isHidden();
+          });
 
-        Patcher.after(UnreadStore, "hasNotableUnread", (_, args, res) => {
-          return res && !ChannelStore.getChannel(args[0])?.isHidden();
-        });
-        
-        Patcher.after(UnreadStore, "hasRelevantUnread", (_, args, res) => {
-          return res && !args[0].isHidden();
-        });
+          Patcher.after(UnreadStore, "hasNotableUnread", (_, args, res) => {
+            return res && !ChannelStore.getChannel(args[0])?.isHidden();
+          });
+          
+          Patcher.after(UnreadStore, "hasRelevantUnread", (_, args, res) => {
+            return res && !args[0].isHidden();
+          });
 
-        Patcher.after(UnreadStore, "hasUnread", (_, args, res) => {
-          return res && !ChannelStore.getChannel(args[0])?.isHidden();
-        });
+          Patcher.after(UnreadStore, "hasUnread", (_, args, res) => {
+            return res && !ChannelStore.getChannel(args[0])?.isHidden();
+          });
 
+          Patcher.after(SomeStore, "hasUnread", (_, args, res) => {
+            return res && !ChannelStore.getChannel(args[0])?.isHidden();
+          });
+
+          Patcher.after(SomeStore, "hasRelevantUnread", (_, args, res) => {
+            if(this.settings.debugMode){console.log("Does this work?")}
+            return res && !args[0].isHidden();
+          });
+        }
 
         //* Make hidden channel visible
         Patcher.after(ChannelPermissionStore, "can", (_, args, res) => {
@@ -273,7 +290,7 @@ module.exports = (() => {
           };
         }
         
-        if (!this.settings.disableIcons){ // && !this.settings.useOgIcons
+        if (!this.settings.disableIcons && !this.settings.useIconsV2){
           var channelChanging = false; // Thanks to vileelf for suggesting a fix for this
           var icon = this.settings.emoji || "🔒";
           Patcher.after(ChannelStore, "getChannel", (thisObject, methodArguments, returnValue) => {
@@ -287,95 +304,55 @@ module.exports = (() => {
               } else {
                 returnValue.name = " " + icon + " " + returnValue.name;
               }
+            }else if (!returnValue?.isHidden?.() && returnValue?.name && returnValue.name.includes(" " + icon + " ")) {
+              returnValue.name = returnValue.name.replace(" " + icon + " ", "");
             }
-  
+
             channelChanging = false;
           }); 
         }
 
-        //! Not working
-        // if (this.settings.useOgIcons && !this.settings.disableIcons) {
-        // console.log("ChannelItem", ChannelItem);
-        // Patcher.after(ChannelItem, "default", (_, args, res) => {
-        //   console.log(args[0].channel);
-        //   const instance = args[0];
-        //   if (instance.channel?.isHidden()) {
-        //     const item = res.props?.children?.props;
+        if (this.settings.useIconsV2 && !this.settings.disableIcons) {
+          const channelInfoSelector = WebpackModules.getByProps("iconVisibility", "channelInfo");
+          const channelNameSelector = WebpackModules.getByProps("channelName", "iconContainer");
+          const iconHTML = `<div aria-label="Hidden Channel" role="img" class="${channelNameSelector.iconItem}"><svg width="24" height="24" class="${channelInfoSelector.actionIcon}" viewBox="0 0 24 24" aria-hidden="true" role="img"><path fill="currentColor" d="M17 11V7C17 4.243 14.756 2 12 2C9.242 2 7 4.243 7 7V11C5.897 11 5 11.896 5 13V20C5 21.103 5.897 22 7 22H17C18.103 22 19 21.103 19 20V13C19 11.896 18.103 11 17 11ZM12 18C11.172 18 10.5 17.328 10.5 16.5C10.5 15.672 11.172 15 12 15C12.828 15 13.5 15.672 13.5 16.5C13.5 17.328 12.828 18 12 18ZM15 11H9V7C9 5.346 10.346 4 12 4C13.654 4 15 5.346 15 7V11Z"></path></svg></div>`
 
-        //     if (item?.className) item.className += ` shc-hidden-channel shc-hidden-channel-type-${instance.channel.type}`;
+          Patcher.after(UnreadStore, "hasUnread", (_, args, retval) => {
+            const allChannels = document.querySelectorAll("#channels > ul > .containerDefault-YUSmu3");
+            
+            if(!allChannels){
+              if(this.settings.debugMode){console.log("No channels found")}
+              return retval;
+            }
 
-        //     const children = res.props?.children?.props?.children[1]?.props?.children[1];
-
-        //     if (children.props?.children) { 
-        //       children.props.children = [
+            allChannels.forEach((channel) => {
+              let c = channel.children[0].lastElementChild.children[1];
               
-        //         React.createElement(Tooltip, {
-        //           text: "Hidden Channel",
-        //         },
+              // Some channels are in a div with no classes wtf
+              if(!c){ c = channel.children[0].children[0].lastElementChild.children[1];}
 
-        //           React.createElement(Clickable,
-        //             {
-        //               className: [iconItem, "shc-lock-icon-clickable"].join(
-        //                 " "
-        //               ),
-        //               style: {
-        //                 display: "block",
-        //               },
-        //             },
+              if(c){
+                let channelID = c.parentElement.firstChild.getAttribute("data-list-item-id");
+                channelID = channelID.replace(/\D/g,'');
 
-        //             // React.createElement(Icon, {
-        //             //   // Icon, {
-        //             //   // name: "Eye", //! no name for stuff so i don't know
-        //             //   // className: actionIcon,
+                if(ChannelStore.getChannel(channelID)?.isHidden?.()){
 
-        //             //   className: [iconBase, actionIcon].join(" "),
-        //             //   style: {
-        //             //     color: "red",
-        //             //     width: "20px",
-        //             //     height: "20px",
-        //             //   },
-        //             // })
-        //             React.createElement('svg', Clickable, children, React.createElement('path', {
-        //               style: {
-        //                 width: "24",
-        //                 height: "24",
-        //                 class: "shc-lock-icon",
-        //                 viewBox: "0 0 24 24",
-        //                 // aria-hidden: "true",
-        //                 role: "img",
-        //               },
-                      
-        //               d: 'M6 6h1v6H6zm3 0h1v6H9z'
-        //             }), React.createElement('path', {
-        //               style: {
-        //                 fill: "currentColor",
-        //               },
-        //               d: 'M17 11V7C17 4.243 14.756 2 12 2C9.242 2 7 4.243 7 7V11C5.897 11 5 11.896 5 13V20C5 21.103 5.897 22 7 22H17C18.103 22 19 21.103 19 20V13C19 11.896 18.103 11 17 11ZM12 18C11.172 18 10.5 17.328 10.5 16.5C10.5 15.672 11.172 15 12 15C12.828 15 13.5 15.672 13.5 16.5C13.5 17.328 12.828 18 12 18ZM15 11H9V7C9 5.346 10.346 4 12 4C13.654 4 15 5.346 15 7V11Z'
-        //             }))
-        //           )
-        //         ),
-        //       ];
-        //     }
+                  if(!c.innerHTML.includes(iconHTML)){
+                    c.innerHTML = c.innerHTML + iconHTML;
+                  }
+                  // c.style.border = "3px solid red"; // Debugging
+                }else{
 
-        //     if (instance.channel.type == DiscordConstants.ChannelTypes.GUILD_VOICE && !instance.connected) {
-        //       const wrapper = Utilities.findInReactTree(res, (n) => n?.props?.className?.includes(ChannelClasses.wrapper));
+                  if(c.innerHTML.includes(iconHTML)){
+                    c.innerHTML = c.innerHTML.replace(iconHTML, "");
+                  }
+                  // c.style.border = "none"; // Debugging
+                }
+              }
 
-        //       if (wrapper) {
-        //         wrapper.props.onMouseDown = () => {};
-        //         wrapper.props.onMouseUp = () => {};
-        //       }
-        //       const mainContent = Utilities.findInReactTree(res, (n) =>
-        //         n?.props?.className?.includes(ChannelClasses.mainContent)
-        //       );
-
-        //       if (mainContent) {
-        //         mainContent.props.onClick = () => {};
-        //         mainContent.props.href = null;
-        //       }
-        //     }
-        //   }
-        //   return res;
-        // });
+            });
+          });
+        }
 
         //! Not working, will be fixed in the future (maybe, idk if i should)
         // ChannelItem.default.displayName = "ChannelItem"; //! No displayName so i don't know what to replace
@@ -402,13 +379,38 @@ module.exports = (() => {
 
       //* Settings
 
+      updateDisabledSettings(what){
+        const settings = what.querySelectorAll(".plugin-input-container")
+
+        if(this.settings.disableIcons){
+          settings[1].style.display = "none";
+          settings[2].style.display = "none";
+          settings[3].style.display = "none";
+        }else{
+          settings[1].style.display = "block";
+          settings[2].style.display = "block";
+          settings[3].style.display = "block";
+        }
+
+        if(this.settings.useIconsV2){
+          settings[2].style.display = "none";
+          settings[3].style.display = "none";
+        }else{
+          settings[2].style.display = "block";
+          settings[3].style.display = "block";
+        }
+      }
+
       getSettingsPanel() {
         const panel = this.buildSettingsPanel();
         panel.addListener(this.updateSettings.bind(this));
+        this.updateDisabledSettings(panel.getElement());
         return panel.getElement();
       }
 
       updateSettings(id, value) {
+        this.updateDisabledSettings(document);
+
         if(id === "emoji") {
 
           if(value.length > 6) { value = value.substring(0, 6); }
