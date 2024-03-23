f43c53e26b642b2ddc5cbd5a4ee351282d525035
JustOptimize <mirko303.staff@gmail.com>
2024-03-24 00:15:29 +0100

    Fix channelicon

--- dist/ShowHiddenChannels.plugin.js
+++ dist/ShowHiddenChannels.plugin.js
@@ -8,6 +8,54 @@
 /******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
+/***/ "./src/components/hiddenChannelIcon.jsx":
+/*!**********************************************!*\
+  !*** ./src/components/hiddenChannelIcon.jsx ***!
+  \**********************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (/* binding */ HiddenChannelIcon)
+/* harmony export */ });
+function _extends() { _extends = Object.assign ? Object.assign.bind() : function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; return _extends.apply(this, arguments); }
+const React = BdApi.React;
+const Tooltip = BdApi.Components.Tooltip;
+function HiddenChannelIcon({
+  icon,
+  iconItem,
+  actionIcon
+}) {
+  return BdApi.React.createElement(Tooltip, {
+    text: "Hidden Channel"
+  }, props => BdApi.React.createElement("div", _extends({
+    className: iconItem,
+    style: {
+      display: 'block'
+    }
+  }, props), icon == 'lock' && BdApi.React.createElement("svg", {
+    className: actionIcon,
+    viewBox: "0 0 24 24"
+  }, BdApi.React.createElement("path", {
+    fill: "currentColor",
+    d: "M17 11V7C17 4.243 14.756 2 12 2C9.242 2 7 4.243 7 7V11C5.897 11 5 11.896 5 13V20C5 21.103 5.897 22 7 22H17C18.103 22 19 21.103 19 20V13C19 11.896 18.103 11 17 11ZM12 18C11.172 18 10.5 17.328 10.5 16.5C10.5 15.672 11.172 15 12 15C12.828 15 13.5 15.672 13.5 16.5C13.5 17.328 12.828 18 12 18ZM15 11H9V7C9 5.346 10.346 4 12 4C13.654 4 15 5.346 15 7V11Z"
+  })), icon == 'eye' && BdApi.React.createElement("svg", {
+    className: actionIcon,
+    viewBox: "0 0 24 24"
+  }, BdApi.React.createElement("path", {
+    fill: "currentColor",
+    d: "M12 5C5.648 5 1 12 1 12C1 12 5.648 19 12 19C18.352 19 23 12 23 12C23 12 18.352 5 12 5ZM12 16C9.791 16 8 14.21 8 12C8 9.79 9.791 8 12 8C14.209 8 16 9.79 16 12C16 14.21 14.209 16 12 16Z"
+  }), BdApi.React.createElement("path", {
+    fill: "currentColor",
+    d: "M12 14C13.1046 14 14 13.1046 14 12C14 10.8954 13.1046 10 12 10C10.8954 10 10 10.8954 10 12C10 13.1046 10.8954 14 12 14Z"
+  }), BdApi.React.createElement("polygon", {
+    fill: "currentColor",
+    points: "22.6,2.7 22.6,2.8 19.3,6.1 16,9.3 16,9.4 15,10.4 15,10.4 10.3,15 2.8,22.5 1.4,21.1 21.2,1.3 "
+  }))));
+}
+
+/***/ }),
+
 /***/ "./src/styles.css":
 /*!************************!*\
   !*** ./src/styles.css ***!
@@ -88,7 +136,9 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
 /* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
 /* harmony export */ });
-/* harmony import */ var _styles_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./styles.css */ "./src/styles.css");
+/* harmony import */ var _components_hiddenChannelIcon__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./components/hiddenChannelIcon */ "./src/components/hiddenChannelIcon.jsx");
+/* harmony import */ var _styles_css__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./styles.css */ "./src/styles.css");
+
 
 
 const config = {
@@ -167,12 +217,12 @@ class MissingZeresDummy {
             type: 'info',
         });
 
-        eval('require')('request').get('https://betterdiscord.app/gh-redirect?id=9', async (err, resp, body) => {
+        BdApi.Net.fetch('https://betterdiscord.app/gh-redirect?id=9', async (err, resp, body) => {
             if (err) return this.downloadZLibErrorPopup();
 
             // If the response is a redirect to the actual file
             if (resp.statusCode === 302) {
-                eval('require')('request').get(resp.headers.location, async (error, response, content) => {
+                BdApi.Net.fetch(resp.headers.location, async (error, response, content) => {
                     if (error) return this.downloadZLibErrorPopup();
                     await new Promise((r) =>
                         eval('require')('fs').writeFile(
@@ -517,7 +567,7 @@ const MyPlugin = (([Pl, Lib]) => {
             }
 
             doStart() {
-                DOMTools.addStyle(config.info.name, _styles_css__WEBPACK_IMPORTED_MODULE_0__["default"]);
+                DOMTools.addStyle(config.info.name, _styles_css__WEBPACK_IMPORTED_MODULE_1__["default"]);
                 this.Patch();
                 this.rerenderChannels();
             }
@@ -665,56 +715,11 @@ const MyPlugin = (([Pl, Lib]) => {
 
                         if (children.props?.children) {
                             children.props.children = [
-                                React.createElement(
-                                    Tooltip,
-                                    {
-                                        text: 'Hidden Channel',
-                                    },
-                                    (props) =>
-                                        React.createElement(
-                                            'div',
-                                            {
-                                                ...props,
-                                                className: `${iconItem}`,
-                                                style: {
-                                                    display: 'block',
-                                                },
-                                            },
-                                            this.settings['hiddenChannelIcon'] == 'lock' &&
-                                                React.createElement(
-                                                    'svg',
-                                                    {
-                                                        class: actionIcon,
-                                                        viewBox: '0 0 24 24',
-                                                    },
-                                                    React.createElement('path', {
-                                                        fill: 'currentColor',
-                                                        d: 'M17 11V7C17 4.243 14.756 2 12 2C9.242 2 7 4.243 7 7V11C5.897 11 5 11.896 5 13V20C5 21.103 5.897 22 7 22H17C18.103 22 19 21.103 19 20V13C19 11.896 18.103 11 17 11ZM12 18C11.172 18 10.5 17.328 10.5 16.5C10.5 15.672 11.172 15 12 15C12.828 15 13.5 15.672 13.5 16.5C13.5 17.328 12.828 18 12 18ZM15 11H9V7C9 5.346 10.346 4 12 4C13.654 4 15 5.346 15 7V11Z',
-                                                    })
-                                                ),
-
-                                            this.settings['hiddenChannelIcon'] == 'eye' &&
-                                                React.createElement(
-                                                    'svg',
-                                                    {
-                                                        class: actionIcon,
-                                                        viewBox: '0 0 24 24',
-                                                    },
-                                                    React.createElement('path', {
-                                                        fill: 'currentColor',
-                                                        d: 'M12 5C5.648 5 1 12 1 12C1 12 5.648 19 12 19C18.352 19 23 12 23 12C23 12 18.352 5 12 5ZM12 16C9.791 16 8 14.21 8 12C8 9.79 9.791 8 12 8C14.209 8 16 9.79 16 12C16 14.21 14.209 16 12 16Z',
-                                                    }),
-                                                    React.createElement('path', {
-                                                        fill: 'currentColor',
-                                                        d: 'M12 14C13.1046 14 14 13.1046 14 12C14 10.8954 13.1046 10 12 10C10.8954 10 10 10.8954 10 12C10 13.1046 10.8954 14 12 14Z',
-                                                    }),
-                                                    React.createElement('polygon', {
-                                                        fill: 'currentColor',
-                                                        points: '22.6,2.7 22.6,2.8 19.3,6.1 16,9.3 16,9.4 15,10.4 15,10.4 10.3,15 2.8,22.5 1.4,21.1 21.2,1.3 ',
-                                                    })
-                                                )
-                                        )
-                                ),
+                                React.createElement(_components_hiddenChannelIcon__WEBPACK_IMPORTED_MODULE_0__["default"], {
+                                    icon: this.settings['hiddenChannelIcon'],
+                                    iconItem: iconItem,
+                                    actionIcon: actionIcon,
+                                }),
                             ];
                         }
 
@@ -1629,19 +1634,21 @@ const MyPlugin = (([Pl, Lib]) => {
                         this.value = isChecked;
                     }
                     onAdded() {
-                        ReactDOM.render(
-                            React.createElement(IconSwitchWrapper, {
-                                icon: this.icon,
-                                children: this.name,
-                                note: this.note,
-                                disabled: this.disabled,
-                                hideBorder: false,
-                                value: this.value,
-                                onChange: (e) => {
-                                    this.onChange(e);
+                        ReactDOM.createRoot(this.getElement()).render(
+                            React.createElement(
+                                IconSwitchWrapper,
+                                {
+                                    icon: this.icon,
+                                    note: this.note,
+                                    disabled: this.disabled,
+                                    hideBorder: false,
+                                    value: this.value,
+                                    onChange: (e) => {
+                                        this.onChange(e);
+                                    },
                                 },
-                            }),
-                            this.getElement()
+                                this.name
+                            )
                         );
                     }
                 }
--- src/components/HiddenChannelIcon.jsx
+++ src/components/HiddenChannelIcon.jsx
@@ -1,40 +1,44 @@
-const React = require('react');
+const React = BdApi.React;
+const Tooltip = BdApi.Components.Tooltip;
 
-export default function HiddenChannelIcon({ icon }) {
+export default function HiddenChannelIcon({ icon, iconItem, actionIcon }) {
     return (
-        <ToolTip text="Hidden Channel">
-            <div
-                className="iconItem"
-                style={{
-                    display: 'block',
-                }}
-            >
-                {icon == 'lock' && (
-                    <svg className="actionIcon" viewBox="0 0 24 24">
-                        <path
-                            fill="currentColor"
-                            d="M17 11V7C17 4.243 14.756 2 12 2C9.242 2 7 4.243 7 7V11C5.897 11 5 11.896 5 13V20C5 21.103 5.897 22 7 22H17C18.103 22 19 21.103 19 20V13C19 11.896 18.103 11 17 11ZM12 18C11.172 18 10.5 17.328 10.5 16.5C10.5 15.672 11.172 15 12 15C12.828 15 13.5 15.672 13.5 16.5C13.5 17.328 12.828 18 12 18ZM15 11H9V7C9 5.346 10.346 4 12 4C13.654 4 15 5.346 15 7V11Z"
-                        />
-                    </svg>
-                )}
+        <Tooltip text="Hidden Channel">
+            {(props) => (
+                <div
+                    className={iconItem}
+                    style={{
+                        display: 'block',
+                    }}
+                    {...props}
+                >
+                    {icon == 'lock' && (
+                        <svg className={actionIcon} viewBox="0 0 24 24">
+                            <path
+                                fill="currentColor"
+                                d="M17 11V7C17 4.243 14.756 2 12 2C9.242 2 7 4.243 7 7V11C5.897 11 5 11.896 5 13V20C5 21.103 5.897 22 7 22H17C18.103 22 19 21.103 19 20V13C19 11.896 18.103 11 17 11ZM12 18C11.172 18 10.5 17.328 10.5 16.5C10.5 15.672 11.172 15 12 15C12.828 15 13.5 15.672 13.5 16.5C13.5 17.328 12.828 18 12 18ZM15 11H9V7C9 5.346 10.346 4 12 4C13.654 4 15 5.346 15 7V11Z"
+                            />
+                        </svg>
+                    )}
 
-                {icon == 'eye' && (
-                    <svg className="actionIcon" viewBox="0 0 24 24">
-                        <path
-                            fill="currentColor"
-                            d="M12 5C5.648 5 1 12 1 12C1 12 5.648 19 12 19C18.352 19 23 12 23 12C23 12 18.352 5 12 5ZM12 16C9.791 16 8 14.21 8 12C8 9.79 9.791 8 12 8C14.209 8 16 9.79 16 12C16 14.21 14.209 16 12 16Z"
-                        />
-                        <path
-                            fill="currentColor"
-                            d="M12 14C13.1046 14 14 13.1046 14 12C14 10.8954 13.1046 10 12 10C10.8954 10 10 10.8954 10 12C10 13.1046 10.8954 14 12 14Z"
-                        />
-                        <polygon
-                            fill="currentColor"
-                            points="22.6,2.7 22.6,2.8 19.3,6.1 16,9.3 16,9.4 15,10.4 15,10.4 10.3,15 2.8,22.5 1.4,21.1 21.2,1.3 "
-                        />
-                    </svg>
-                )}
-            </div>
-        </ToolTip>
+                    {icon == 'eye' && (
+                        <svg className={actionIcon} viewBox="0 0 24 24">
+                            <path
+                                fill="currentColor"
+                                d="M12 5C5.648 5 1 12 1 12C1 12 5.648 19 12 19C18.352 19 23 12 23 12C23 12 18.352 5 12 5ZM12 16C9.791 16 8 14.21 8 12C8 9.79 9.791 8 12 8C14.209 8 16 9.79 16 12C16 14.21 14.209 16 12 16Z"
+                            />
+                            <path
+                                fill="currentColor"
+                                d="M12 14C13.1046 14 14 13.1046 14 12C14 10.8954 13.1046 10 12 10C10.8954 10 10 10.8954 10 12C10 13.1046 10.8954 14 12 14Z"
+                            />
+                            <polygon
+                                fill="currentColor"
+                                points="22.6,2.7 22.6,2.8 19.3,6.1 16,9.3 16,9.4 15,10.4 15,10.4 10.3,15 2.8,22.5 1.4,21.1 21.2,1.3 "
+                            />
+                        </svg>
+                    )}
+                </div>
+            )}
+        </Tooltip>
     );
 }
